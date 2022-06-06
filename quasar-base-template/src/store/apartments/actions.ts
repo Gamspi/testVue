69d4dca02ac9fbc02f21925595a14568331ab525
Gamspi <niko.tatt007@gmail.com>
2022-06-06 17:41:20 +0700

    рефакторинг

--- quasar-base-template/src/store/apartments/actions.ts
+++ quasar-base-template/src/store/apartments/actions.ts
@@ -2,28 +2,13 @@ import { ActionTree } from 'vuex'
 import { StateInterface } from '../index'
 import { ApartmentsStateInterface } from './state'
 import LoadService from '../../services/LoadService'
-// import axios from 'axios'
 
 const actions: ActionTree<ApartmentsStateInterface, StateInterface> = {
-  // fetchApartments ({ commit }) {
-  //   commit('mutateIsLoading', true)
-  //   setTimeout(() => {
-  //     axios.get('./apartments.json')
-  //       .then(({ data }) => {
-  //         commit('mutateApartments', data)
-  //       })
-  //       .catch((e) => alert(e))
-  //       .finally(() => {
-  //         commit('mutateIsLoading', false)
-  //       })
-  //   }, 3000)
-  // },
-  async fetchApartments ({ commit }):Promise<void> {
-    commit('mutateIsLoading', true)
 
+  async sfetchApartments ({ commit }):Promise<void> {
+    commit('mutateIsLoading', true)
     await LoadService.apiRequest(('./apartments.json')).then(
       ({ data }) => {
-        console.log(data)// eslint-disable-line
         commit('mutateApartments', data)
       }
     )
@@ -31,7 +16,6 @@ const actions: ActionTree<ApartmentsStateInterface, StateInterface> = {
         commit('mutateIsLoading', false)
       })
   }
-
 }
 
 export default actions
